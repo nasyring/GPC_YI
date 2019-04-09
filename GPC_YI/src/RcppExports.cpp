94a2de1799416e824cc2e3d4bdb16264ea59bb00
nasyring <nasyrin@gmail.com>
2019-04-09 15:27:30 -0500

    Update RcppExports.cpp

--- GPC_YI/src/RcppExports.cpp
+++ GPC_YI/src/RcppExports.cpp
@@ -116,6 +116,38 @@ BEGIN_RCPP
 END_RCPP
 }    
 
+// rcpp_parallel_yi smooth prior data
+NumericVector rcpp_parallel_smoothp_yi(NumericVector nn, NumericMatrix data1, NumericMatrix data2, NumericMatrix thetaboot, NumericVector bootmean0,
+	NumericVector bootmean1, NumericMatrix databoot1, NumericMatrix databoot2, NumericMatrix priordata1, NumericMatrix priordata2, NumericVector priorweight, NumericVector scheduleLen, NumericMatrix propSched, NumericVector ddelta, NumericVector alpha, NumericVector M_samp, NumericVector B_resamp, 
+	NumericVector w);
+RcppExport SEXP GPCYI_rcpp_parallel_smoothp_yi(SEXP nnSEXP, SEXP data1SEXP, SEXP data2SEXP, SEXP thetabootSEXP, SEXP bootmean0SEXP,
+	SEXP bootmean1SEXP, SEXP databoot1SEXP, SEXP databoot2SEXP, SEXP priordata1SEXP, SEXP priordata2SEXP, SEXP priorweightSEXP, SEXP scheduleLenSEXP, SEXP propSchedSEXP, SEXP ddeltaSEXP, SEXP alphaSEXP, SEXP M_sampSEXP, SEXP B_resampSEXP, 
+	SEXP wSEXP){
+BEGIN_RCPP
+    Rcpp::RObject __result;
+    Rcpp::RNGScope __rngScope;
+    Rcpp::traits::input_parameter< NumericVector >::type nn(nnSEXP);
+    Rcpp::traits::input_parameter< NumericMatrix >::type data1(data1SEXP);
+    Rcpp::traits::input_parameter< NumericMatrix >::type data2(data2SEXP);
+    Rcpp::traits::input_parameter< NumericMatrix >::type thetaboot(thetabootSEXP);
+    Rcpp::traits::input_parameter< NumericVector >::type bootmean0(bootmean0SEXP);
+    Rcpp::traits::input_parameter< NumericVector >::type bootmean1(bootmean1SEXP);
+    Rcpp::traits::input_parameter< NumericMatrix >::type databoot1(databoot1SEXP);
+    Rcpp::traits::input_parameter< NumericMatrix >::type databoot2(databoot2SEXP);
+    Rcpp::traits::input_parameter< NumericMatrix >::type priordata1(priordata1SEXP);
+    Rcpp::traits::input_parameter< NumericMatrix >::type priordata2(priordata2SEXP);
+    Rcpp::traits::input_parameter< NumericVector >::type priorweight(priorweightSEXP);
+    Rcpp::traits::input_parameter< NumericVector >::type scheduleLen(scheduleLenSEXP);
+    Rcpp::traits::input_parameter< NumericMatrix >::type propSched(propSchedSEXP);
+    Rcpp::traits::input_parameter< NumericVector >::type ddelta(ddeltaSEXP);
+    Rcpp::traits::input_parameter< NumericVector >::type alpha(alphaSEXP);
+    Rcpp::traits::input_parameter< NumericVector >::type M_samp(M_sampSEXP);
+    Rcpp::traits::input_parameter< NumericVector >::type B_resamp(B_resampSEXP);
+    Rcpp::traits::input_parameter< NumericVector >::type w(wSEXP);
+    __result = Rcpp::wrap(rcpp_parallel_smoothp_yi(nn, data1, data2, thetaboot, bootmean0, bootmean1, databoot1, databoot2, priordata1, priordata2, priorweight, scheduleLen, propSched, ddelta, alpha, M_samp, B_resamp, w));
+    return __result;
+END_RCPP
+}  
 
 // rcpp_parallel_yi
 NumericVector rcpp_parallel_yi_kde(NumericVector nn, NumericMatrix data, NumericVector nnp, NumericMatrix priordata, NumericVector priorweight, NumericMatrix thetaboot, NumericVector bootmean0,
@@ -174,6 +206,30 @@ BEGIN_RCPP
     Rcpp::traits::input_parameter< NumericVector >::type M_samp(M_sampSEXP);
     Rcpp::traits::input_parameter< NumericVector >::type w(wSEXP);
     __result = Rcpp::wrap(GibbsMCMC2smooth(nn, data1, data2, bootmean0, bootmean1, normprior, scheduleLen, propSched, alpha, ddelta, M_samp, w));
+    return __result;
+END_RCPP
+}
+
+Rcpp::List GibbsMCMCp2smooth(NumericVector nn, NumericMatrix data1, NumericMatrix data2, NumericVector bootmean0, NumericVector bootmean1, NumericMatrix priordata1, NumericMatrix priordata2, NumericVector priorweight, NumericVector scheduleLen, NumericMatrix propSched, NumericVector alpha, NumericVector ddelta, NumericVector M_samp, NumericVector w);
+RcppExport SEXP GPCYI_GibbsMCMCp2smooth(SEXP nnSEXP, SEXP data1SEXP, SEXP data2SEXP, SEXP bootmean0SEXP, SEXP bootmean1SEXP, SEXP priordata1SEXP, SEXP priordata2SEXP, SEXP priorweightSEXP, SEXP scheduleLenSEXP, SEXP propSchedSEXP, SEXP alphaSEXP, SEXP ddeltaSEXP, SEXP M_sampSEXP, SEXP wSEXP){
+BEGIN_RCPP
+    Rcpp::RObject __result;
+    Rcpp::RNGScope __rngScope;
+    Rcpp::traits::input_parameter< NumericVector >::type nn(nnSEXP);
+    Rcpp::traits::input_parameter< NumericMatrix >::type data1(data1SEXP);
+    Rcpp::traits::input_parameter< NumericMatrix >::type data2(data2SEXP);
+    Rcpp::traits::input_parameter< NumericVector >::type bootmean0(bootmean0SEXP);
+    Rcpp::traits::input_parameter< NumericVector >::type bootmean1(bootmean1SEXP);
+    Rcpp::traits::input_parameter< NumericMatrix >::type priordata1(priordata1SEXP);
+    Rcpp::traits::input_parameter< NumericMatrix >::type priordata2(priordata2SEXP);
+    Rcpp::traits::input_parameter< NumericVector >::type priorweight(priorweightSEXP);
+    Rcpp::traits::input_parameter< NumericVector >::type scheduleLen(scheduleLenSEXP);
+    Rcpp::traits::input_parameter< NumericMatrix >::type propSched(propSchedSEXP);
+    Rcpp::traits::input_parameter< NumericVector >::type ddelta(ddeltaSEXP);
+    Rcpp::traits::input_parameter< NumericVector >::type alpha(alphaSEXP);
+    Rcpp::traits::input_parameter< NumericVector >::type M_samp(M_sampSEXP);
+    Rcpp::traits::input_parameter< NumericVector >::type w(wSEXP);
+    __result = Rcpp::wrap(GibbsMCMCp2smooth(nn, data1, data2, bootmean0, bootmean1, priordata1, priordata2, priorweight, scheduleLen, propSched, alpha, ddelta, M_samp, w));
     return __result;
 END_RCPP
 }
@@ -242,9 +298,11 @@ static const R_CallMethodDef CallEntries[] = {
     {"GPCYI_GridSearchKDE", (DL_FUNC) &GPCYI_GridSearchKDE, 11},
     {"GPCYI_rcpp_parallel_yi", (DL_FUNC) &GPCYI_rcpp_parallel_yi, 15},
     {"GPCYI_rcpp_parallel_smooth_yi", (DL_FUNC) &GPCYI_rcpp_parallel_smooth_yi, 16},
+    {"GPCYI_rcpp_parallel_smoothp_yi", (DL_FUNC) &GPCYI_rcpp_parallel_smoothp_yi, 18},
     {"GPCYI_rcpp_parallel_yi_kde", (DL_FUNC) &GPCYI_rcpp_parallel_yi_kde, 22},
     {"GPCYI_GibbsMCMC2", (DL_FUNC) &GPCYI_GibbsMCMC2, 13},
     {"GPCYI_GibbsMCMC2smooth", (DL_FUNC) &GPCYI_GibbsMCMC2smooth, 12},
+    {"GPCYI_GibbsMCMCp2smooth", (DL_FUNC) &GPCYI_GibbsMCMC2smooth, 14},
     {"GPCYI_GibbsMCMCkde2", (DL_FUNC) &GPCYI_GibbsMCMCkde2, 20},
     {NULL, NULL, 0}
 };
